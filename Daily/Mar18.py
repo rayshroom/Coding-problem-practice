0e68e976f14d7c45ad19b21f2b981703213bf1cf
DESKTOP-O9QCJ6N\rqyu <goodcolorpencils@gmail.com>
2021-03-18 15:50:45 -0400

    mar18 daily

---
+++ Daily/Mar18.py
@@ -0,0 +1,29 @@
+'''
+Given an integer array nums, return the length of the longest wiggle sequence.
+
+A wiggle sequence is a sequence where the differences between successive numbers strictly alternate between positive and negative. The first difference (if one exists) may be either positive or negative. A sequence with fewer than two elements is trivially a wiggle sequence.
+
+For example, [1, 7, 4, 9, 2, 5] is a wiggle sequence because the differences (6, -3, 5, -7, 3) are alternately positive and negative.
+In contrast, [1, 4, 7, 2, 5] and [1, 7, 4, 5, 5] are not wiggle sequences, the first because its first two differences are positive and the second because its last difference is zero.
+A subsequence is obtained by deleting some elements (eventually, also zero) from the original sequence, leaving the remaining elements in their original order.
+
+
+Note: sequence is not consecutive
+'''
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 1
+        prev = nums[1] - nums[0]
+        count = 1
+        if prev != 0:
+            count += 1
+            
+        for i in range(2, len(nums)):
+            curr = nums[i] - nums[i-1]
+            if (curr > 0 and prev <= 0) or (curr < 0 and prev >= 0):
+                count += 1
+                prev = curr
+                
+        return count
